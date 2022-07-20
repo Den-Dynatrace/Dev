e81c16d58a430dda6669193631d3d0863933850e
esundbla <69823637+esundbla@users.noreply.github.com>
2022-07-20 10:00:42 -0600

    Finall inject doc code

--- routes/injectDoc.js
+++ routes/injectDoc.js
@@ -1,23 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: false}));
 const {inject} = require('../db_queries.js')
+var user = "erik.sundblad"
 
 /* GET Inject view */
 router.get('/', function(req, res, next) {
   res.render('injectDoc');
 });
 
-router.post('/injectDoc', function (req, res) {
-    doc = {
-    "Overall" : req.body.Overall,
-    "Sub-Cat" : req.body.SubCat,
-    "metric" : req.body.metric,
-    "value" : req.body.value,
-    "Proof" : req.body.Proof,
-    "Notes" : req.body.notes,
-    "Tag" : req.body.Tag
+router.post('/', async function (req, res, next) {
+  var overall = await req.body.subject;
+  var subcat = await req.body.topic;
+  var metric = await req.body.chapter;
+  var tag = req.body.tags;
+  var proof = await req.body.proof
+  var notes = await req.body.Comment
+  doc = {
+    "Overall" : overall,
+    "Sub-Cat" : subcat,
+    "metric" : metric,
+    "value" : req.body.val,
+    "Proof" : proof,
+    "Notes" : notes,
+    "Tag" : tag
     }
-    inject(user, doc)
+    console.log(doc)
+    //inject(user, doc)
+    //res.redirect('/')
   })
 
 module.exports = router;
